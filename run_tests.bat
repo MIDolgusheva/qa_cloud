41f111511551c474bb19592773f2e96b07fb8bf8
MIDolgusheva <90573951+MIDolgusheva@users.noreply.github.com>
2023-06-21 12:16:52 +0300

    Update run_tests.bat

--- run_tests.bat
+++ run_tests.bat
@@ -1,3 +1,3 @@
-pip install -r ./requirements.txt
-pytest -s -v --alluredir=test_results/ tests
-allure serve test_results/
+pip install -r requirements.txt
+pytest -s -v --alluredir=test_results\ tests
+allure serve test_results\
